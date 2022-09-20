df8ff55a1ee94f024e32490b7a34ec0de7efee39
c-liming <c-liming@qq.com>
2022-09-21 00:03:18 +0800

    vscode win

--- miniprogram_server/login/views.py
+++ miniprogram_server/login/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from email.policy import default
+from django.shortcuts import render, HttpResponse
 from .models import *
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
@@ -9,5 +11,11 @@ def login_views(request):
         return render(request, "login/html/login.html") 
 
     elif request.method == "POST":
-        
+        try:
+            uid = Account.objects.get(uid=request.POST.get('uid'))
+            upw = Account.objects.get(upw=request.POST.get('upw'))
+        except ObjectDoesNotExist:
+            return HttpResponse("账号或密码错误%s %s" % (request.POST.get('uid'), request.POST.get('upw')))
+
+        print('test', uid, " ", upw)
         return render(request, "login/html/login.html")
