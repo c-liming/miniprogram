753df1f394abde14c4ab36cabe599762de79c99d
c-liming <c-liming@qq.com>
2022-09-23 23:27:52 +0800

    vscode win

--- miniprogram_server/login/models.py
+++ miniprogram_server/login/models.py
@@ -1,3 +1,4 @@
+from time import ctime
 from django.db import models
 
 # Create your models here.
@@ -13,3 +14,18 @@ class Account(models.Model):
 
     def __str__(self):
         return '%s' % self.nickname
+
+
+
+
+class WebmasterStatistical(models.Model):
+
+    # 站长统计
+
+    remote_addr = models.CharField('IPv4', max_length=15)
+    path_info = models.URLField('访问地址', max_length=200)
+    ctime = models.DateTimeField('创建时间', auto_now_add=True)
+    atime = models.DateTimeField('修改时间', auto_now=True)
+
+    def __str__(self):
+        return 'IP:%s_addr:%s' % (self.remote_addr, self.path_info)
--- miniprogram_server/miniprogram_server/settings.py
+++ miniprogram_server/miniprogram_server/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.mymiddleware.MyMW'
 ]
 
 ROOT_URLCONF = 'miniprogram_server.urls'
